c430ffb43e233e214258850c1c2b2ce69d323704
Mat <106345025+nocush@users.noreply.github.com>
2023-03-12 18:45:02 +0100

    Added Lab4_1

---
+++ Lab4/Lab4_1/Lab4_1/ContentView.swift
@@ -0,0 +1,80 @@
+//
+//  ContentView.swift
+//  Lab4_1
+//
+//  Created by Mateusz Bartoszek on 12/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    private var currencies: [String: Double] = ["PLN": 1, "USD": 4.4, "EUR": 4.68, "GBO": 5.29]
+    @State private var sourceCurrency:String = "PLN"
+    @State private var destinationCurrency: String = "PLN"
+    @State private var amount: Int = 1
+    @State private var showHours: Bool = false
+    @State private var showContact: Bool = false
+    var body: some View {
+        VStack {
+            Text("Kantor")
+                .frame(maxWidth: .infinity)
+                .padding()
+                .foregroundColor(.white)
+                .background(Color.gray)
+                .font(.largeTitle)
+            Spacer()
+            
+            Group{
+                Text("Kupowana waluta")
+                
+                TextField("Wartość kupowanej waluty",text: Binding(get: {String(self.amount)}, set: {self.amount = abs(Int($0) ?? self.amount)}))
+                    .multilineTextAlignment(.center)
+                    .padding(.top)
+                
+                Picker("Kupowana waluta", selection: $sourceCurrency){
+                    ForEach(Array(currencies.keys),id: \.self){
+                        key in Text(key)
+                    }
+                }.labelsHidden()
+                
+            }
+            Divider().padding()
+            Group{
+                Text("Płatność za pomocą")
+                
+                Picker("Płatność za pomocą",selection: $destinationCurrency){
+                    ForEach(Array(currencies.keys),id: \.self){ key in Text(key)}
+                }.labelsHidden()
+                
+                Text("Do zapłaty: \(self.currencies[self.sourceCurrency]! / self.currencies[self.destinationCurrency]! * Double(self.amount),specifier: "%.2f") \(self.destinationCurrency)")
+            }
+            Spacer()
+            HStack{
+                Button(action:{self.showHours = true}){
+                    Image(systemName: "clock").resizable().frame(width:40,height:40)
+                }
+                .alert(isPresented:$showHours){
+                    Alert(title: Text("Godziny otwarcia"),message: Text("Pn-Pt: 9:00-21:00"))
+                }
+                
+                Button(action:{ self.showContact = true}){
+                    Image(systemName: "person.circle").resizable()
+                        .frame(width:40,height:40)
+                }
+                .alert(isPresented:$showContact){
+                    Alert(title: Text("Kontakt"),message:
+                        Text("kantor@gmail.com")
+                    )
+                }
+            }
+            
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lab4/Lab4_1/Lab4_1/Lab4_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab4_1App.swift
+//  Lab4_1
+//
+//  Created by Mateusz Bartoszek on 12/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Lab4_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
